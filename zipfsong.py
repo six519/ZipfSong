a7d74294b84815ab104928a304127c3de5fb0855
six519 <ferdinandsilva@ferdinandsilva.com>
2013-07-05 19:58:03 +0800

    fix bug

--- zipfsong.py
+++ zipfsong.py
@@ -1,52 +1,19 @@
 #!/usr/bin/env python
-import sys
-
-class ZipFSong(object):
-
-
-    def run(self):
-
-        try:
-
-            line = sys.stdin.readline().strip()
-            song = ()
-            qualityList = []
-            selc = 0
-
-            splittedInput = line.split(" ")
-            songToBeSelected = long(splittedInput[1])
-
-            for n in xrange(long(splittedInput[0])):
-
-                line = sys.stdin.readline().strip()
-
-                splittedInput = line.split(" ")
-
-                quality = long(splittedInput[0]) * (n + 1)
-                #song.append()
-                song += ((str(splittedInput[1]), quality),)
-                if quality not in qualityList:
-                    qualityList.append(quality)
-            
-            while selc != songToBeSelected:
-                
-                #max_num = mxsort(qualityList)
-                #max_num = sorted(qualityList)[-1]
-                max_num = max(qualityList)
-
-                for i, nn in enumerate(qualityList):
-                    if nn == max_num:
-                        del qualityList[i]
-
-                for s in song:
-                    if s[1] == max_num:
-                        selc += 1
-                        sys.stdout.write(s[0] + "\n")
-                        #print s[0]
-            return 0
-
-        except Exception as e:
-            return -1
+"""
+ :) :) :) :)
+"""
 
 if __name__ == "__main__":
-    ZipFSong().run()
+
+    inputs = []
+    while 1:
+
+        try:
+            inputs.append(raw_input())
+        except Exception:
+            break
+
+    songs = [{'played': int(inpt.split()[0]),'nm':inpt.split()[1], 'quality': int(inpt.split()[0]) * (i + 1)} for i, inpt in enumerate(inputs[1:int(inputs[0].split()[0]) + 1])]
+    result = sorted(songs, key=lambda song:song['quality'], reverse=True)[:int(inputs[0].split()[1])]
+    print "\n".join([s['nm'] for s in result])
+
